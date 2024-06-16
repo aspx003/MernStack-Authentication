860316918abc902c6a8b4cfadf36145eee0eed7e
Ansuman Patra <ansupatra003@gmail.com>
2024-06-16 07:12:29 +0530

    Controllers and routes(single route) added and tested with postman. More routes to add.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
 
 const port = process.env.PORT || 5000;
 
 const app = express();
-
-
+app.use('/api/users', userRoutes);
 app.get('/', (req, res) => res.send('Sever is ready!'));
 
 
