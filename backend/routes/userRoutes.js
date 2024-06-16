0256fef6da0108149adbeea2594adedd564dfdae
Ansuman Patra <ansupatra003@gmail.com>
2024-06-16 07:15:17 +0530

    Routing added and tested with postman

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { authUser } from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.post('/auth', authUser);
+
+export default router;
