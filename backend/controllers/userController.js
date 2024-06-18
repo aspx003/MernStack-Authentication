6e96228c3ec969c5ca9a6f7fc7dfc344c2fdd043
Ansuman Patra <ansupatra003@gmail.com>
2024-06-18 08:27:13 +0530

    Authorization and registration completed and tested out.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,44 +1,81 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc	Auth user/set token
 // route    POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Auth User' });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid Login Data");
+  }
 });
 
 // @desc	Register a new user
 // route    POST /api/users/
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Register User' });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid Error Data.");
+  }
 });
 
 // @desc	Register a new user
 // route    POST /api/users/
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Logout User' });
+  res.status(200).json({ message: "Logout User" });
 });
 
 // @desc	Register a new user
 // route    POST /api/users/
 // @access  Public
 const getUserProfile = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Get User Profile' });
+  res.status(200).json({ message: "Get User Profile" });
 });
 
 // @desc	Update User profile
 // route    PUT /api/users/profile
 // @access  Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: 'Update User Profile' });
+  res.status(200).json({ message: "Update User Profile" });
 });
 
 export {
-	authUser,
-	registerUser,
-	logoutUser,
-	getUserProfile,
-	updateUserProfile
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
 };
--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,13 @@ import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 
+const app = express();
 connectDB();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 100
 const port = process.env.PORT || 5000;
 
-const app = express();
+
 app.use('/api/users', userRoutes);
 app.get('/', (req, res) => res.send('Sever is ready!'));
 
