a87009e2608c935cb172f19b2159512fd10fd08a
Ansuman Patra <ansupatra003@gmail.com>
2024-06-18 08:53:02 +0530

    Authoriazation protection added Require a better testing client.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User Logged out" });
 });
 
 // @desc	Register a new user
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+dotenv.config();
 
+// setup
+const port = process.env.PORT || 5000;
 const app = express();
 connectDB();
+
+// middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 100
-const port = process.env.PORT || 5000;
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-
-app.use('/api/users', userRoutes);
-app.get('/', (req, res) => res.send('Sever is ready!'));
+// routing
+app.use("/api/users", userRoutes);
+app.get("/", (req, res) => res.send("Sever is ready!"));
 
 app.use(notFound);
 app.use(errorHandler);
 
-
 app.listen(port, () => {
-	console.log(`Sever started at port ${port}`);
-})
+  console.log(`Sever started at port ${port}`);
+});
