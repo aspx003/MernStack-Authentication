7b2d1dbdf33bcbea44fdfc78457e4471197066a2
Ansuman Patra <ansupatra003@gmail.com>
2024-06-23 07:16:52 +0530

    Fixed issue of cookies not getting recognized. Finished the getUserProfile.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,7 +67,12 @@ const logoutUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/
 // @access  Public
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 // @desc	Update User profile
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,6 +11,7 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.userId).select("-password");
+      next();
     } catch (err) {
       res.status(401);
       throw new Error("Not authorized, invalid token");
