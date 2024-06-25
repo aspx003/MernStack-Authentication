d3428d0799cc02a19419f1eb2116f465258498bd
Ansuman Patra <ansupatra003@gmail.com>
2024-06-25 19:25:56 +0530

    Router Setup Complete

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,29 @@
 // import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand>MERN Auth</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>MERN Auth</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link>
-                <FaSignInAlt /> Sign In
-              </Nav.Link>
-              <Nav.Link>
-                <FaSignOutAlt /> Sign Up
-              </Nav.Link>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaSignInAlt /> Sign In
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/register">
+                <Nav.Link>
+                  <FaSignOutAlt /> Sign Up
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { Container, Card, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Hero = () => {
   return (
@@ -12,10 +13,14 @@ const Hero = () => {
             Bootstrap library
           </p>
           <div className="d-flex">
-            <Button variant="primary" className="me-3">
-              Sign In
-            </Button>
-            <Button variant="secondary">Register</Button>
+            <LinkContainer to="/login">
+              <Button variant="primary" className="me-3">
+                Sign In
+              </Button>
+            </LinkContainer>
+            <LinkContainer to="/register">
+              <Button variant="secondary">Sign Up</Button>
+            </LinkContainer>
           </div>
         </Card>
       </Container>
